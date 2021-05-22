c356c1fe3463b7f5f61aea3e8f0b7bfebdeefd16
shyclown <roman.moravcik1@gmail.com>
2021-05-22 18:35:46 +0200

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-Bootstraped with create-react-app
-
-can be started by running 
-`npm install` and `npm run start`
-
-You should see app on localhost:4444
-
-![Image of App](./src/main/Assignment.png)
--- src/utils/resources/localStorage.ts
+++
@@ -1,8 +0,0 @@
-export const saveObject = (key: string, value: {[key: string]: any}) => {
-    localStorage.setItem(key, JSON.stringify(value))
-}
-
-export const loadObject = (key: string) => {
-    const value = localStorage.getItem(key);
-    return value ? JSON.parse(value) : {};
-}
